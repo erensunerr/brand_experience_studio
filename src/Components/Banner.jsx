39a8471774820973861279a575dc5ff67a6be09b
Eren <gerensuner@gmail.com>
2022-07-18 14:01:28 +0300

    build issues resolved

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion'
-import {useState, } from 'react';
 
 import './banner.css'
 import Image from './Image'
--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,4 +1,3 @@
-import {useEffect} from 'react';
 import { motion } from 'framer-motion';
 
 import Image from './Image';
